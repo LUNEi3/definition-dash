85f8ee45becd38a634ab624b0473f57bd3f06737
LUNEi3 <niranon.49@gmail.com>
2025-11-28 19:37:46 +0700

    Fix touching area of Get Started button

--- app/_components/Welcome.tsx
+++ app/_components/Welcome.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Welcome() {
     return  (
-        <section className="bg-white h-[65%] p-8 w-full self-end flex flex-col justify-around rounded-t-[3rem] drop-shadow-[0px_10px_40px_rgba(0,0,0,0.25)]">
+        <section className="bg-white h-[65%] p-8 w-full text-black text-center self-end flex flex-col justify-around rounded-t-[3rem] drop-shadow-[0px_10px_40px_rgba(0,0,0,0.25)]">
             <div className="py-10">
                 <h1 className="text-black text-center text-3xl font-extrabold">
                     Welcome To Definition Dash
@@ -12,10 +12,10 @@ export default function Welcome() {
                     A classic mutiple-choice format, but the pressure is on the clock.
                 </p>
             </div>
-            <div className="py-10">
-                <button className="bg-[#AAC4F5] text-white py-5 w-full rounded-full">
-                    <Link href={"/sign-in"}>Get Start</Link>
-                </button>
+            <div className="flex flex-col">
+                <Link href={"/sign-in"} className="bg-[#AAC4F5] text-white py-5 w-full rounded-full">
+                    Get Start
+                </Link>
                 <p className="text-black tracking-wide py-3 text-center text-sm font-medium opacity-40">
                     ALREADY HAVE AN ACCOUTN? <span className="text-[#AAC4F5]">LOG IN</span>
                 </p>
