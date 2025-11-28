2f97998f7e9b1079c5983fbce5a006fa927c6a37
LUNEi3 <niranon.49@gmail.com>
2025-11-28 19:39:23 +0700

    Change redirect to add query params into URL

--- app/sign-in/action.tsx
+++ app/sign-in/action.tsx
@@ -1,9 +1,14 @@
 'use server'
 
+import { encodeData } from '@/lib/utils/urlHelpers'
 import { redirect } from 'next/navigation'
 
 export async function submitData(formData: FormData) {
-
     const name = formData.get('name') as string || "Guest"
-    redirect(`/home?name=${encodeURIComponent(name)}&veiw=default`)
+
+    const userData = encodeData({
+        name
+    })
+    
+    redirect(`/home?q=${userData}&view=default`)
 }
