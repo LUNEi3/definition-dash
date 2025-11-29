44c2cffb10b4a6e0e5a695bdba128847a39cd5d9
LUNEi3 <niranon.49@gmail.com>
2025-11-29 22:41:13 +0700

    Implement /leaderboard route page

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,28 @@
+import { decodeData } from "@/lib/utils/urlHelpers";
+import Link from "next/link";
+
+
+export default async function Leaderboard({ searchParams } : { searchParams: { q?: string }} ) {
+    const { q } = await searchParams;
+    const userData = decodeData(q ?? "") || {};
+    const backUrl = `/home?q=${q}&view=default`;
+
+    return (
+        <main className="bg-white">
+            <section className='flex flex-col p-8 h-screen justify-center'>
+                <div className='text-black text-center'>
+                    <h1 className='text-3xl font-extrabold py-5'>
+                        Your Score is {userData.score}!
+                    </h1>
+                    <p className='text-sm font-light opacity-60'>
+                        The leaderboard feature is under inplementation...
+                    </p>
+                </div>
+                <Link href={backUrl} className='bg-[#AAC4F5] text-white text-center my-5 py-5 w-full rounded-full'>
+                    Back
+                </Link>
+
+            </section>
+        </main>
+    );
+}
